aa0cafb1c545b7abe5735f6c2a5ccc894aaa4f41
Laserbean <laserbean00001@gmail.com>
2022-01-13 21:06:41 +1300

    initial commit

---
+++ globalstuff.cs
@@ -0,0 +1,41 @@
+
+
+public enum Matter
+{
+    Solid,
+    Powder,
+    Liquid,
+    Gas, 
+    Other,
+    None,
+    Bedrock
+}
+
+public class Constants
+{
+    public const float GRAVITY = 0.5f;  //pixels per frame
+    public const int CHUNK_SIZE = 16;
+    public const float PIXEL_SCALE = 0.125f;
+    public const float PERIOD = 0.05f;
+    public const int seed = 123;
+
+
+    // public const float PERIOD = 0.5f;
+}
+
+public enum e_name {
+    //Solids
+    Bedrock, Stone, 
+
+    //Powders,
+    Sand,
+    //Liquids
+    Water, Oil,
+
+    //Gas,
+    Smoke, 
+
+    //Other
+    Nothing
+    
+}
